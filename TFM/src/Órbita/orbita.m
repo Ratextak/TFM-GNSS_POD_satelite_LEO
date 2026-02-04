10174ceb4cd5e29f04862207cf30029b15ca56b4
Ratextak <estrefugaz12@gmail.com>
2026-02-04 10:33:00 +0100

    Eliminados los parámetros de la deriva y cambios en el tiempo de muestreo de la órbita

--- TFM/src/Órbita/orbita.m
+++ TFM/src/Órbita/orbita.m
@@ -3,20 +3,27 @@ rutaResultado = "results/Órbita_Matlab/";
 tle = "data/tle_UPMSat-2.tle";
 
 % Creamos el escenario donde estará nuestro satélite (1 día de simulación o 1 órbita).
-tiempoInicio = datetime(2023, 1, 20, 10, 6, 27);
-tiempoFin = tiempoInicio + minutes(60*24);  % 1 día de simulación.
-tiempoMuestreo = 10;  % 10s entre las muestras.
+%tiempoInicio = datetime(2023, 1, 20, 10, 6, 27);
+%tiempoFin = tiempoInicio + minutes(60*24);  % 1 día de simulación.
+%tiempoMuestreo = 10;  % 10s entre las muestras.
 %escenario = satelliteScenario(tiempoInicio, tiempoFin, tiempoMuestreo);
 escenario = satelliteScenario();  % 1 órbita.
 
 % Creamos el satélite y su órbita con el fichero TLE.
 sat = satellite(escenario, tle);
+escenario.SampleTime = 0.01;  % Tiempo de muestreo = 10 ms (como en Spirent).
 
 % Muestra los elementos orbitales al inicio de la epoch por pantalla.
 elemOrbitales0 = orbitalElements(sat)
 
-% Preparamos las entradas para Spirent. Las ' trasponen las matrices.
+% Calculamos los tiempos para cada muestra.
 tiempos = escenario.StartTime : seconds(escenario.SampleTime) : escenario.StopTime;
+% Debemos meter otra muestra si no coinciden, ya que la función states lo tiene en cuenta.
+if tiempos(end) ~= escenario.StopTime  
+    tiempos(end+1) = escenario.StopTime;
+end
+
+% Preparamos las entradas para Spirent. Las ' trasponen las matrices.
 segundos = seconds(tiempos - tiempos(1))';  % Convertimos tipo datetime a segundos desde el inicio de la simulación.
 veh_mot = createArray(length(tiempos), 1, FillValue={'v1_m1'});
 lla = states(sat, CoordinateFrame="geographic")';  % Lon (°), lat (°) y alt (m).
@@ -43,22 +50,20 @@ elemOrbClasicos = [a', e', rad2deg(i'), rad2deg(O'), rad2deg(o'), rad2deg(M')];
 % Unimos las entradas en una tabla y escribimos el fichero .txt.
 % Comando MOTB.
 MOTB = createArray(length(tiempos), 1, FillValue={'MOTB'});
-ceros = zeros(length(tiempos), 1);  % Para la deriva, que no la vamos a utilizar.
 T_MOTB = table(segundos, MOTB, veh_mot, lat, lon, alt, vNorth, vEast, -vUp, ...
-    ace_enu(:,2), ace_enu(:,1), -ace_enu(:,3), ceros, ceros, ceros, ...
+    ace_enu(:,2), ace_enu(:,1), -ace_enu(:,3), ...
     VariableNames=["Tiempo", "Comando", "Veh_mot", "Latitud", "Longitud", "Altitud", ...
-    "Velocidad N", "Velocidad E", "Velocidad D", "Aceleración N", "Aceleración E", "Aceleración D", ...
-    "Deriva N", "Deriva E", "Deriva D"]);
+    "Velocidad N", "Velocidad E", "Velocidad D", "Aceleración N", "Aceleración E", "Aceleración D"]);
 writetable(T_MOTB, rutaResultado+"orbita_UPMSat2_MOTB.txt", WriteMode="overwrite");
+disp("---> Archivo MOTB escrito");
 % Comando MOT.
 MOT = createArray(length(tiempos), 1, FillValue={'MOT'});
 T_MOT = table(segundos, MOT, veh_mot, pos_ecef(:,1), pos_ecef(:,2), pos_ecef(:,3), ...
-    vel_ecef(:,1), vel_ecef(:,2), vel_ecef(:,3), ace_ecef(:,1), ace_ecef(:,2), ...
-    ace_ecef(:,3), ceros, ceros, ceros, ...
+    vel_ecef(:,1), vel_ecef(:,2), vel_ecef(:,3), ace_ecef(:,1), ace_ecef(:,2), ace_ecef(:,3), ...
     VariableNames=["Tiempo", "Comando", "Veh_mot", "Posición X", "Posición Y", "Posición Z", ...
-    "Velocidad X", "Velocidad Y", "Velocidad Z", "Aceleración X", "Aceleración Y", "Aceleración Z", ...
-    "Deriva X", "Deriva Y", "Deriva Z"]);
+    "Velocidad X", "Velocidad Y", "Velocidad Z", "Aceleración X", "Aceleración Y", "Aceleración Z"]);
 writetable(T_MOT, rutaResultado+"orbita_UPMSat2_MOT.txt", WriteMode="overwrite");
+disp("---> Archivo MOT escrito");
 
 % Creamos la vista en 3D de la órbita de nuestro satélite.
 vista = satelliteScenarioViewer(escenario, position=[450 250 700 700]);
